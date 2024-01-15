2011c3e4bc78499427ea43ce6f6ad7f6594bf854
Azam Al Deen <122336794+AzamAlDeen@users.noreply.github.com>
2024-01-15 23:49:56 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:localstorage/localstorage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'update_location.dart'; // Import the update_location.dart file
 
-void main() {
+void main() 
+{
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatelessWidget 
+{
   const MainApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      theme: ThemeData
+      (
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
@@ -22,39 +29,49 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatefulWidget 
+{
   const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  final LocalStorage storage = LocalStorage('geofence_app');
+class _HomePageState extends State<HomePage> 
+{
   List<Map<String, double>> locations = [];
   String message = '';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar
+      (
         title: Text('Geofencing Application'),
         centerTitle: true, // Center the title
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
+      body: Column
+      (
+        children: <Widget>
+        [
+          Padding
+          (
             padding: EdgeInsets.only(top: 20), // Add space between AppBar and container
-            child: Container(
+            child: Container
+            (
               width: 400, // Set width of the container
               height: 100, // Set height of the container
               padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
+              decoration: BoxDecoration
+              (
                 color: Color.fromARGB(255, 223, 223, 223),
                 borderRadius: BorderRadius.circular(10), // Add rounded corners
               ),
-              child: Center( // Center the text inside the container
-                child: Text(
+              child: Center
+              ( // Center the text inside the container
+                child: Text
+                (
                   'App Instructions: Press the button to get and save your current location.', 
                   style: TextStyle(fontSize: 15),
                   textAlign: TextAlign.center, // Center align the text
@@ -62,24 +79,36 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Expanded(
-            child: Center(
-              child: Column(
+          Expanded
+          (
+            child: Center
+            (
+              child: Column
+              (
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
+                children: <Widget>
+                [
+                  ElevatedButton
+                  (
                     child: Text('Get and Save Current Location'),
-                    onPressed: () async {
+                    onPressed: () async 
+                    {
                       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
                       Map<String, double> location = {'latitude': position.latitude, 'longitude': position.longitude};
-                      storage.setItem('location', location);
-                      setState(() {
+                      SharedPreferences prefs = await SharedPreferences.getInstance();
+                      List<String> locationsList = prefs.getStringList('locations') ?? [];
+                      locationsList.add(jsonEncode(location));
+                      prefs.setStringList('locations', locationsList);
+                      setState(() 
+                      {
                         locations.add(location);
                         message = 'Your location has been located';
                       });
 
-                      Future.delayed(Duration(seconds: 2), () {
-                        setState(() {
+                      Future.delayed(Duration(seconds: 2), () 
+                      {
+                        setState(() 
+                        {
                           message = '';
                         });
                       });
@@ -90,32 +119,44 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Container(
+          Container
+          (
             padding: EdgeInsets.only(bottom: 100),
-            child: Column(
-              children: <Widget>[
+            child: Column
+            (
+              children: <Widget>
+              [
                 Text('Saved Locations', style: TextStyle(fontSize: 24)),
-                Padding(
+                Padding
+                (
                   padding: EdgeInsets.only(bottom: 10), // Add space below the Container
-                  child: Container(
+                  child: Container
+                  (
                     height: 300, // Adjust this value to make the DataTable bigger
                     width: 400,
                     padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
+                    decoration: BoxDecoration
+                    (
                       color: Color.fromARGB(255, 223, 223, 223),
                       borderRadius: BorderRadius.circular(50), // Add rounded corners
                     ),
-                    child: DataTable(
-                      columns: const <DataColumn>[
-                        DataColumn(
+                    child: DataTable
+                    (
+                      columns: const <DataColumn>
+                      [
+                        DataColumn
+                        (
                           label: Text('Latitude'),
                         ),
-                        DataColumn(
+                        DataColumn
+                        (
                           label: Text('Longitude'),
                         ),
                       ],
-                      rows: locations.reversed.take(5).map((location) => DataRow(
-                        cells: <DataCell>[
+                      rows: locations.reversed.take(3).map((location) => DataRow
+                      (
+                        cells: <DataCell>
+                        [
                           DataCell(Text(location['latitude'].toString())),
                           DataCell(Text(location['longitude'].toString())),
                         ],
@@ -123,13 +164,16 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                ElevatedButton(
-                  child: Text('Delete All Locations'),
-                  onPressed: () {
-                    storage.clear();
-                    setState(() {
-                      locations.clear();
-                    });
+                ElevatedButton
+                (
+                  child: Text('Go to Update Location Screen'),
+                  onPressed: () 
+                  {
+                    Navigator.push
+                    (
+                      context,
+                      MaterialPageRoute(builder: (context) => UpdateLocationScreen()),
+                    );
                   },
                 ),
               ],
